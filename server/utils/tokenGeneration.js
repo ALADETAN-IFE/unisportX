f9cf109f710f3764fc75101dcc97ce006a44e700
ALADETAN-IFE <famousfotune123@gmail.com>
2025-07-07 20:21:59 +0100

    Updated token generation to set SameSite attribute to 'None' for cross-domain compatibility in production environments, enhancing cookie handling for user sessions.

--- server/utils/tokenGeneration.js
+++ server/utils/tokenGeneration.js
@@ -19,7 +19,7 @@ exports.generateToken = (user, res) => {
         httpOnly: true,
         secure:  process.env.NODE_ENV === 'production', 
         maxAge: 60 * 60 * 24 * 7, // 7 days
-        sameSite: 'strict',
+        sameSite: 'None',
         path: '/',
         // sameSite: isProduction && !isLocalhost ? 'none' : 'lax', // Use 'none' for cross-domain in production
         // domain: isProduction && !isLocalhost ? undefined : undefined // Let browser handle domain
