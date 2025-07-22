ec533b70a345dd4a268687bf69cb0b201fcd3aab
ALADETAN-IFE <famousfotune123@gmail.com>
2025-07-22 15:11:42 +0100

    still debugging 3

--- client/api/sitemap.js
+++ client/api/sitemap.js
@@ -1,4 +1,3 @@
-//   const response = await fetch("https://unisportx.onrender.com/sitemap.xml");
 export default async function handler(req, res) {
   const serverUrl = process.env.VITE_SERVER_URL;
   const response = await fetch(`${serverUrl}/sitemap.xml`);
